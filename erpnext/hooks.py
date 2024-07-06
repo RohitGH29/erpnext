5aca38a17b02f48fe26187e8c6f84019f7ae3056
rohit <rohit.d@softwaremathematics.com>
2024-07-06 11:04:22 +0000

    ho

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -1,12 +1,12 @@
 app_name = "erpnext"
-app_title = "ERPNext"
-app_publisher = "Frappe Technologies Pvt. Ltd."
+app_title = "SM ERP (Next Generation ERP)"
+app_publisher = "Software Mathematics Pvt. Ltd."
 app_description = """ERP made simple"""
 app_icon = "fa fa-th"
 app_color = "#e74c3c"
-app_email = "info@erpnext.com"
+app_email = "info@softwaremathematics.com"
 app_license = "GNU General Public License (v3)"
-source_link = "https://github.com/frappe/erpnext"
+source_link = "https://github.com/Software-Mathematics/SMERP"
 app_logo_url = "/assets/erpnext/images/erpnext-logo.svg"
 
 
